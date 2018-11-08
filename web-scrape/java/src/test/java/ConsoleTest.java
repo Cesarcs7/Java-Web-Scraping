f7a128a8ce2a3e57bb5cd39338b13aa2766f766d
unknown <cesar_cs_71@hotmail.com>
2018-11-08 16:07:21 +0100

    added Console Class Unit Tests

--- web-scrape/java/src/test/java/ConsoleTest.java
+++ web-scrape/java/src/test/java/ConsoleTest.java
@@ -1,14 +1,25 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class ConsoleTest {
+    private List<String> list = new ArrayList<>();
+    private List<String> finalPosibleList = new ArrayList<>();
 
     @Test
     public void mergeMatchesLists() {
+        Console console = new Console();
+        List<String> mergedLists = console.mergeMatchesLists();
+        assertEquals(list, mergedLists);
     }
 
     @Test
     public void finalFilter() {
+        Console console = new Console();
+        List<String> finalList = console.finalFilter(finalPosibleList);
+        assertEquals(list, finalList);
     }
 }
